025299ab3afbf472e2f695cd9aad90d3ad32c474
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-08-23 05:10:37 +0600

    skill section complete and animation added

--- src/Pages/Skill/Skill.jsx
+++ src/Pages/Skill/Skill.jsx
@@ -1,12 +1,64 @@
 import React from 'react';
 import PageHeaderContent from '../../PageHeaderContent/PageHeaderContent';
 import { GiSkills } from 'react-icons/gi'
+import './Skill.scss'
+import { Animate ,AnimateKeyframes } from 'react-simple-animate';
+import skillApi from './skill';
+import { Line } from 'rc-progress';
+
 
 const Skill = () => {
   return (
-    <section id='skill' className='skill'>
-      <PageHeaderContent headerText ='My Skill' icon={<GiSkills size={40}/>}></PageHeaderContent>
+    <section id='skills' className='skills'>
+      <PageHeaderContent headerText ='My Skills' icon={<GiSkills size={40}/>}></PageHeaderContent>
       
+      <div className='skills__content__wrapper'>
+        {
+          skillApi.map((item , i)=>(
+            <div key={i} className='skills__content__wrapper__inner-content'>
+                <Animate 
+                play
+                duration={1}
+                delay={0.3}
+                start={{
+                  transform : 'translateX(-200px)'
+                }}
+                end={{
+                  transform : 'translateX(0px)'
+                }}
+                >
+ 
+                  <h3 className='skills__content__wrapper__inner-content__category-text'>{item.label}</h3>
+                  
+                  <div className='skills__content__wrapper__inner-content__progressbar-container'>
+                 {
+                   item.data.map((skillItem,j)=>(
+                     <AnimateKeyframes
+                     play
+                     duration={1}
+                     keyframes={["opacity : 1" , "opacity : 0"]}
+                     iterationCount = "1"
+                      >
+                       <div className='progressbar-wrapper' key={j}>
+                         <p>{skillItem.skillName}</p>
+                         <Line
+                         percent = {skillItem.percentage}
+                         strokeWidth='2'
+                         strokeColor='var(--yellow-theme-main-color)'
+                          trailWidth="2"
+                          strokeLinecap='square'
+                         ></Line>
+                       </div>
+                     </AnimateKeyframes>
+                   ))
+                 }
+               </div>
+                
+             </Animate>
+            </div>
+          ))
+        }
+      </div>
     </section>
   );
 };
---
+++ src/Pages/Skill/skill.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+const skillApi = [
+  {
+    label:"FRONT END",
+    data:[
+      {
+        skillName:"HTML",
+        percentage:'90'
+      },
+      {
+        skillName:"CSS",
+        percentage:'90'
+      },
+      {
+        skillName:"JAVASCRIPT",
+        percentage:'80'
+      },
+      {
+        skillName:"REACT",
+        percentage:'80'
+      },
+    ]
+  },
+  {
+    label:"BACKEND END",
+    data:[
+      {
+        skillName:"NODE JS",
+        percentage:'80'
+      },
+      {
+        skillName:"EXPRESS JS",
+        percentage:'80'
+      },
+      {
+        skillName:"REACT QUERY",
+        percentage:'60'
+      },
+      {
+        skillName:"NEXT JS",
+        percentage:'60'
+      },
+    ]
+  },
+  {
+    label:"DATA BASE",
+    data:[
+      {
+        skillName:"MONGODB",
+        percentage:'80'
+      },
+      {
+        skillName:"FIREBASE",
+        percentage:'80'
+      }
+    ]
+  },
+  // {
+  //   label:"WORDPRESS",
+  //   data:[
+  //     {
+  //       skillName:"ELEMENTOR",
+  //       percentage:'90'
+  //     },
+  //     {
+  //       skillName:"WOODMART",
+  //       percentage:'60'
+  //     },
+  //     {
+  //       skillName:"WOCOMMERCE",
+  //       percentage:'50'
+  //     }
+  //   ]
+  // }
+  
+]
+
+export default skillApi;
