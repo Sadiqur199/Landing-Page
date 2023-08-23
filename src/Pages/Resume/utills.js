e7ce1ad6ce935a6e1a0c4beff6b23313711145c7
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-08-23 20:57:38 +0600

    work the resume section

---
+++ src/Pages/Resume/utills.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const utills =[
+  experience : [
+   {
+    title: 'web development',
+    subTitle : 'developer',
+    description : 'User Experience , Visual Design'
+   },
+   {
+    title: 'web development',
+    subTitle : 'developer',
+    description : 'User Experience , Visual Design'
+   },
+   {
+    title: 'web development',
+    subTitle : 'developer',
+    description : 'User Experience , Visual Design'
+   }
+  ],
+  education : [
+   {
+    title: 'web development',
+    subTitle : 'developer',
+    description : 'User Experience , Visual Design'
+   },
+   {
+    title: 'web development',
+    subTitle : 'developer',
+    description : 'User Experience , Visual Design'
+   },
+   {
+    title: 'web development',
+    subTitle : 'developer',
+    description : 'User Experience , Visual Design'
+   }
+  ]
+
+]
+
+export default utills;
