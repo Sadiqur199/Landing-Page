56932ffdfb6c1621acda60fc091f83a9f1c66d78
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-08-28 04:31:18 +0600

    update resume section

--- src/Pages/Resume/utills.js
+++ src/Pages/Resume/utills.js
@@ -19,19 +19,19 @@ const utills ={
   ],
   education : [
     {
+      title: 'B.SC IN CSE',
+      subTitle : 'Bangladesh University Business & Technology',
+      description : 'Major Software Engineering.Study Complete(2023)'
+     },
+     {
+      title: ' Higher Secondary School Certificate',
+      subTitle : 'Kazipur Govt  Mansur Ali College',
+      description : 'Background Science.Study Complete(2019)'
+     },
+    {
      title: 'Secondary School Certificate',
      subTitle : 'puranpur High School',
      description : 'Background Science.Study Complete(2017)'
-    },
-    {
-     title: ' Higher Secondary School Certificate',
-     subTitle : 'Kazipur Govt  Mansur Ali College',
-     description : 'Background Science.Study Complete(2019)'
-    },
-    {
-     title: 'B.SC IN CSE',
-     subTitle : 'Bangladesh University Business & Technology',
-     description : 'Major Software Engineering.Study Complete(2023)'
     }
    ]
 
